4a3ecc7da001b1888a6c29f31ea6e756469cf6aa
DINETHKUMAR <dinethkumardk@gmail.com>
2025-02-19 16:13:08 +0530

    used-DOM

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let calculation = '';
+
+let calculation = localStorage.getItem('calculation') || '';
+answer();
+
+console.log(localStorage.getItem('calculation'));
 
       function updateCalculation(value) {
-        calculation += value;
+        calculation += String(value);
         console.log(calculation);
-      }
+   
+        localStorage.setItem('calculation',calculation);
+        answer();
+      };
+      function saveCalculation() {
+        localStorage.setItem('calculation', calculation);
+        answer();
+      };
+      function answer(){
+        document.querySelector('.js-ans')
+         .innerHTML = `Answer: ${calculation}`;
+      };
